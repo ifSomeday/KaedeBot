6066f056b0de9560d6df81f4ec1f601e82cb530b
Will <willrice21@gmail.com>
2017-05-02 23:40:26 -0400

    clean up for pull
    merging with main, then branching again for  the beginning of steam
    integration

--- bernoulli_detection.py
+++ bernoulli_detection.py
@@ -1,3 +1,5 @@
+##UNUSED CURRENTLY
+
 import face_recognition
 import asyncio, aiohttp, os, re, pickle
 from PIL import Image
@@ -20,6 +22,7 @@ def dumpTable():
 async def checkImage(attachments):
     for attachment in attachments:
         url = attachment.get("url")
+        print("image downloaded")
         return(await checkUrlImage(url))
 
 async def checkUrlImage(url):
@@ -28,22 +31,27 @@ async def checkUrlImage(url):
     path = fileNameRE.search(str(path)).group(1)
     filepath = os.getcwd() + "/" + path
     print(filepath)
+    print("filepath determined")
     async with aiohttp.get(url) as r:
         img = await r.read()
         with open(filepath, "wb") as f:
             f.write(img)
             print("image written")
-        res = await recognize(filepath)
-        os.remove(filepath)
-        return(res)
+    r.close()
+    f.close()
+    res = await recognize(filepath)
+    print("result returned")
+    os.remove(filepath)
+    print("filed removed")
     return(res)
 
 async def recognize(filename):
     unknown = face_recognition.load_image_file(filename)
-    try:
-        unk_enc = face_recognition.face_encodings(unknown)[0]
-        results = face_recognition.compare_faces([bern_enc], unk_enc)
+    print("image loaded")
+    unk_enc = face_recognition.face_encodings(unknown)
+    print("image encoded")
+    if(len(unk_enc) > 0):
+        results = face_recognition.compare_faces([bern_enc], unk_enc[0])
+        print("image compared")
         return(results[0])
-    except:
-        return(False)
-    return("lol")
+    return(False)
--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import markovChaining, saveThread, keys, BSJ, os
 import asyncio
 import random, time
 import bernoulli_detection
+import edit_distance as ed
+
+testingEnabled= False
+
+
+
+
+
+##fixes race conditions lol
+time.sleep(15)
 
 ##Praw
 client = discord.Client()
@@ -43,7 +53,7 @@ async def processMessage(client, message):
 
     if message.content.startswith("!help"):
         await client.send_message(message.channel,
-                            "Use !meme to get a spicy meme\nUse !newmeme to add your own dank memes")
+                            "Use !meme to get a spicy meme\nUse !newmeme to add your own dank memes\nUse !MsjMe to get BSJ memes\nUse !BsjName to get your BSJ Name\nUse !twitter to see my twitter counterpart")
 
     if message.content.startswith("!BsjMe"):
         await client.send_message(message.channel, BsjFacts.getFact())
@@ -54,31 +64,11 @@ async def processMessage(client, message):
     if message.content.startswith("!twitter"):
         await client.send_message(message.channel, "Follow my twitter counterpart!!\nhttps://twitter.com/NameIsBot")
 
-    if message.content.startswith('Can I get a "what what" from my homies?!'):
+    if(ed.distance(message.content.lower(), 'can I get a "what what" from my homies?!') < 6):
         if(not str(message.author.id) == str(85148771226234880)):
             await client.send_message(message.channel, "what what")
         else:
-            await client.send_message(message.channel, "nope")
-
-    if message.content.startswith("!test"):
-        url = message.content[len("!test"):]
-        await bernoulli_detection.checkUrlImage("https://cdn.discordapp.com/attachments/213086692683415552/302667850697408523/berger.jpg")
-
-    if(not message.content.startswith('!')):
-        ##if(random.randint(0,1000) == 666):
-        # if(message.author.name.lower() == "lay your heaven on me"):
-        #     newNick = message.author.name + " senpai san"
-        #     print(newNick)
-        #     client.change_nickname(message.author, newNick)
-        #     await client.send_message(message.channel, "(✿◠‿◠)")
-        pass
-
-    if len(message.attachments) != 0:
-        res = await bernoulli_detection.checkImage(message.attachments)
-        print(res)
-        if(res == True):
-            await client.send_message(message.channel, "Illicit Bernoulli detected!\nPlease be more careful in the future.")
-
+            await client.send_message(message.channel, "quack quack")
 
 
 @client.event
