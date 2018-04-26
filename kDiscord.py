5a3fee6143baeb3820d4c200a38d55e17036b3ba
Will Rice <willrice21@gmail.com>
2018-04-26 14:54:56 -0600

    Disable draft, disable lobby create

--- kDiscord.py
+++ kDiscord.py
@@ -402,6 +402,7 @@ def discBot(kstQ, dscQ, factoryQ, draftEvent):
 
     async def create_lobby(*args, **kwargs):
         if('msg' in kwargs):
+            return
             msg = kwargs['msg']
             ##TODO: verify here
             ##TODO: get name
