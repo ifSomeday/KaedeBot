a5687afcbc23356acdcc278c3fa973f667706541
Will Rice <willrice21@gmail.com>
2020-10-07 13:53:43 -0700

    Update misc.py
    add error logging

--- cogs/misc.py
+++ cogs/misc.py
@@ -77,12 +77,14 @@ class Misc(commands.Cog):
         
         await self.poll.add_reaction(self.bot.get_emoji(253705709596704769))
         await self.poll.add_reaction(self.bot.get_emoji(253705749937520640))
-
-        if(self.pollLoop.is_running()):
-            await ctx.send("Previous petition on name `{}` has been canceled.".format(self.proposedName))
-            self.pollLoop.restart()
-        else:
-            self.pollLoop.start()         
+        try:       
+            if(self.pollLoop.is_running()):
+                await ctx.send("Previous petition on name `{}` has been canceled.".format(self.proposedName))
+                self.pollLoop.restart()
+            else:
+                self.pollLoop.start()      
+        except Exception as e:
+            print(e)   
 
     @rename.error
     async def renameError(self, ctx, error):
@@ -91,22 +93,25 @@ class Misc(commands.Cog):
 
     @tasks.loop(seconds=15, count=60)
     async def pollLoop(self):
-        if(not self.poll == None):
-            self.poll = await self.poll.channel.fetch_message(self.poll.id)
-            reactions = self.poll.reactions
-            vote = 0
-            for r in reactions:
-                if r.emoji.id == 253705709596704769:
-                    vote += r.count
-                elif r.emoji.id == 253705749937520640:
-                    vote -= r.count
-            print("Current vote = {}".format(vote))
-            if(vote > 2):
-                await self.poll.channel.edit(name=self.proposedName)
-                await self.poll.send("Petition passed! Channel name changed to `{}`".format(self.proposedName))
-                self.poll = None
-                self.proposedName = ""
-                self.pollLoop.cancel()
+        try:
+            if(not self.poll == None):
+                self.poll = await self.poll.channel.fetch_message(self.poll.id)
+                reactions = self.poll.reactions
+                vote = 0
+                for r in reactions:
+                    if r.emoji.id == 253705709596704769:
+                        vote += r.count
+                    elif r.emoji.id == 253705749937520640:
+                        vote -= r.count
+                print("Current vote = {}".format(vote))
+                if(vote > 2):
+                    await self.poll.channel.edit(name=self.proposedName)
+                    await self.poll.send("Petition passed! Channel name changed to `{}`".format(self.proposedName))
+                    self.poll = None
+                    self.proposedName = ""
+                    self.pollLoop.cancel()
+        except Exception as e:
+            print(e)
 
     @pollLoop.after_loop
     async def afterPollLoop(self):
